bb4cef0fd1b901489b61c91ae0d94e18163c9c95
Trang <nguyenminhtrang9213@gmail.com>
2026-08-28 17:49:33 +0200

    add layout for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <section>Moon Fest Shop</section>
+      <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Hero from "../components/Hero";
+import Layout from "../components/Layout";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Layout>
+        <Hero />
+      </Layout>
+    </div>
+  );
+}
